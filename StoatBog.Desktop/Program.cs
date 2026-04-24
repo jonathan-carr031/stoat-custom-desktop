4dd4901a58ab8213bd7c3e520e365c59abfe0fb3
Jonathan <jonathan.carr031@gmail.com>
2026-04-24 11:55:24 -0400

    Updated to prevent duplicate instances opening

--- StoatBog.Desktop/Program.cs
+++ StoatBog.Desktop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Avalonia;
 using AvaloniaApplication1;
 
@@ -6,14 +7,24 @@ namespace StoatBog.Desktop;
 
 internal sealed class Program
 {
+    private static readonly Mutex Mutex = new(true, "A6669AD0-DB69-4340-96ED-2CFBF04AA35D");
+
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
+        if (!Mutex.WaitOne(TimeSpan.Zero, true))
+            // Another instance is already running
+            return;
+
+
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
+
+        GC.KeepAlive(Mutex);
     }
 
 
