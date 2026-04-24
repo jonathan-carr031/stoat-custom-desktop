4cce02f636af2a09e0a63950ff418b462a55ddce
Jonathan <jonathan.carr031@gmail.com>
2026-04-24 11:36:20 -0400

    Removing unused code

--- StoatBog.Desktop/Program.cs
+++ StoatBog.Desktop/Program.cs
@@ -1,26 +1,17 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Avalonia;
 using AvaloniaApplication1;
-using Updatum;
 
 namespace StoatBog.Desktop;
 
 internal sealed class Program
 {
-    private static readonly UpdatumManager AppUpdater = new("jonathan-carr031", "stoat-custom-desktop")
-    {
-        InstallUpdateWindowsInstallerArguments = "/qb" // Displays a basic user interface for MSI package
-    };
-
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
-        // _ = CheckForUpdates();
-
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
     }
@@ -32,33 +23,4 @@ internal sealed class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
-
-    private static async Task CheckForUpdates()
-    {
-        try
-        {
-            var updateFound = await AppUpdater.CheckForUpdatesAsync();
-            if (!updateFound) return;
-
-            // Optional show a message to the user with the changelog
-            Console.WriteLine("Changelog:");
-            Console.WriteLine(AppUpdater.GetChangelog());
-
-            var downloadedAsset = await AppUpdater.DownloadUpdateAsync();
-
-            if (downloadedAsset == null)
-            {
-                Console.WriteLine("Failed to download the update.");
-                return;
-            }
-
-            // You can manually handle the installation or call the installation method:
-            // Returns false if the installation failed, otherwise it will never return true as the process will be terminated to complete the installation.
-            await AppUpdater.InstallUpdateAsync(downloadedAsset);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
-    }
 }
