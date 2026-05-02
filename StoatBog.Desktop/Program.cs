d5bee64447e99aaec95de9d13a9d03dce64565e6
Jonathan <jonathan.carr031@gmail.com>
2026-05-02 13:18:28 -0400

    Updating logic to properly display and update ui for session value

--- StoatBog.Desktop/Program.cs
+++ StoatBog.Desktop/Program.cs
@@ -16,11 +16,13 @@ internal sealed class Program
     [STAThread]
     public static void Main(string[] args)
     {
+#if !DEBUG
         if (!Mutex.WaitOne(TimeSpan.Zero, true))
         {
             // Another instance is already running
             return;
         }
+#endif
 
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
--- StoatBog/ViewModels/MainViewModel.cs
+++ StoatBog/ViewModels/MainViewModel.cs
@@ -1,13 +1,40 @@
 ﻿using System;
-using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace AvaloniaApplication1.ViewModels;
 
-public partial class MainViewModel : ViewModelBase
+public class MainViewModel : INotifyPropertyChanged
 {
-    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+    private DateTime _sessionExpirationDate;
 
-    public DateTime SessionExpirationDate = DateTime.Now;
-    public string SessionExpirationDateString => SessionExpirationDate.ToString("MM/dd/yyyy HH:mm tt");
+
+    public DateTime SessionExpirationDate
+    {
+        get => _sessionExpirationDate;
+        set
+        {
+            _sessionExpirationDate = value;
+            OnPropertyChanged(nameof(SessionExpirationDateString));
+            OnPropertyChanged(nameof(IsSessionExpired));
+        }
+    }
+
+    public string SessionExpirationDateString => SessionExpirationDate.ToString("MM/dd/yyyy hh:mm tt");
     public bool IsSessionExpired => SessionExpirationDate > DateTime.Now;
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 }
